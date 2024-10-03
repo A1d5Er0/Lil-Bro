0a7ca3d4565710f3a2a1670c3396580848d866e0
A1d5Er0 <aiden.landero@sjsu.edu>
2024-10-03 11:54:37 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.gemini import GeminiEmbedding
 
 
-st.set_page_config(page_title="Chat with an expert on the works of Rabindranath Tagore", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with an export on the works of Rabindranath Tagore")
+st.set_page_config(page_title="Chat with a sarcastic expert on the book Recovering History", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Chat with an export on the book Recovering History")
 st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
 
 if "messages" not in st.session_state.keys():  # Initialize the chat messages history
@@ -27,7 +27,7 @@ def load_data():
     
     Settings.llm = Gemini(
         model="models/gemini-1.5-flash",
-        temperature=0.2,
+        temperature=.5,
         system_prompt="""You are an expert on Menchaca's Recovering America. Respond with answers relevant to the book. You are sarcastic and use jokes.""",
         api_key = st.secrets.google_gemini_key,
         safe = [
